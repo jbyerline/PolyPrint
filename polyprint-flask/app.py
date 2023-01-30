9ffc80fa548bf218f057ab350af8e0070e63d192
Jacob Byerline <jbyerline@gmail.com>
2023-01-29 19:43:20 -0800

    further simplify

--- polyprint-flask/app.py
+++ polyprint-flask/app.py
@@ -3,69 +3,11 @@ from flask_cors import CORS
 import json
 import os
 import sys
-from apscheduler.schedulers.background import BackgroundScheduler
-import asyncio
-import time
-
-import filter
 import utils
-import logging
 
 # TODO: Make this dynamic? Maybe an env var?
 CONFIG_PATH = "printerConfigLocal.json"
-
-
 # CONFIG_PATH = "printerConfig.json"
-
-# TODO: This logic may no longer be necessary if handled by js code
-def check_for_online_printers():
-    tic = time.perf_counter()
-    # print("Checking for printer connectivity...")
-    data = utils.read_config(CONFIG_PATH)
-    printer_addresses = []
-    for printer in data['printers']:
-        if "privateIp" in printer and "publicUrl" in printer:
-            printer_addresses.append(
-                {
-                    "name": printer["name"],
-                    "urls": [printer["privateIp"], printer["publicUrl"]]
-                }
-            )
-        elif "privateIp" in printer:
-            printer_addresses.append(
-                {
-                    "name": printer["name"],
-                    "urls": [printer["privateIp"]]
-                }
-            )
-        elif "publicUrl" in printer:
-            printer_addresses.append(
-                {
-                    "name": printer["name"],
-                    "urls": [printer["publicUrl"]]
-                }
-            )
-    results = asyncio.run(utils.gather(printer_addresses))
-    for printer_status in results:
-        printer = [printer for printer in data['printers'] if (printer_status['name'] in list(printer.values()))][0]
-        if any(printer_status['statuses']):
-            if not printer['isOnline']:
-                print(printer['name'] + " is now online!")
-            printer['isOnline'] = True
-        else:
-            if printer['isOnline']:
-                print(printer['name'] + " is now offline!")
-            printer['isOnline'] = False
-    utils.save_config(CONFIG_PATH, data)
-    toc = time.perf_counter()
-    # print(f"It took {toc - tic:0.4f} seconds to check all printers")
-
-
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(check_for_online_printers, 'interval', seconds=1)
-scheduler.start()
-my_filter = filter.NoRunningFilter()
-logging.getLogger("apscheduler.scheduler").addFilter(my_filter)
 
 app = Flask(__name__)
 CORS(app)
@@ -84,13 +26,6 @@ def login():
         return "Login Success", 200
     else:
         return "Login Failed", 401
-
-
-@app.route('/test', methods=['GET'])
-def test():
-    files = os.listdir(os.curdir)  # files and directories
-    print(files, file=sys.stderr)
-    return "", 200
 
 
 @app.route('/', methods=['GET'])
--- polyprint-flask/filter.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-
-class NoRunningFilter(logging.Filter):
-    def filter(self, record):
-        return not ("maximum number of running instances reached" in record.msg)
